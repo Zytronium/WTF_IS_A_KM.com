01481cef8437ef96e94fbd7136ad36ae2645c3d7
Zytron1228 <90411599+Zytron1228@users.noreply.github.com>
2025-01-16 22:58:47 -0600

    changed video to an image and audio to help make autoplay easier.

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,14 @@
 console.log('🦅WHAT THE FUCK IS A KILOMETER??');
 
-const video = document.getElementById('video');
+const image = document.getElementById('meme');
+const audio = document.getElementById('audio');
 
 // Close the tab when the video ends
-video.addEventListener('ended', () => {
+audio.addEventListener('ended', () => {
     window.close();
 });
 
 // In case autoplay fails
-video.addEventListener('click', () => {
-    video.play();
+image.addEventListener('click', () => {
+    audio.play();
 })
-
-// Fallback: Close the tab after 6 seconds if the video doesn't end
-setTimeout(() => {
-    if (!video.ended) {
-        window.close();
-    }
-}, 6000);
