09b6783a03664bf39edb88f7e2776cf0ac3d39d9
zytronium <dastelljes@outlook.com>
2025-06-28 18:15:41 -0500

    Added invisible heading to help with SEO and fixed audio not re-playing after clicking if autoplay was not blocked and page didn't close when audio finished.

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,18 @@ console.log('🦅WHAT THE FUCK IS A KILOMETER??');
 document.addEventListener('DOMContentLoaded', () => {
     const image = document.getElementById('meme');
     const audio = document.getElementById('audio');
-    
-    // Play it
+
+    // Try to autoplay the audio
     audio.play().catch(err => {
-        meme.addEventListener("click", async () => {
-            audio.play();
-        })
+        // Instruct user to click to play because autoplay was blocked
         alert("Your browser blocked autoplay. Please click to play.");
     });
-    
+
+    // Play/replay audio on click
+    image.addEventListener("click", async () => {
+        audio.play();
+    })
+
     // Close the tab when the audio ends
     audio.addEventListener('ended', () => {
         window.close();
