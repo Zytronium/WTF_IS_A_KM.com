5465c752d449b1ff5acdcdc01b8d3170b8045b56
zytronium <dastelljes@outlook.com>
2025-06-27 23:53:42 -0500

    Added an alert to notify users that autoplay was blocked

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 console.log('🦅WHAT THE FUCK IS A KILOMETER??');
 
-const image = document.getElementById('meme');
-const audio = document.getElementById('audio');
-
-audio.play();
-
-// Close the tab when the audio ends
-audio.addEventListener('ended', () => {
-    window.close();
+document.addEventListener('DOMContentLoaded', () => {
+    const image = document.getElementById('meme');
+    const audio = document.getElementById('audio');
+    
+    // Play it
+    audio.play().catch(err => {
+        meme.addEventListener("click", async () => {
+            audio.play();
+        })
+        alert("Your browser blocked autoplay. Please click to play.");
+    });
+    
+    // Close the tab when the audio ends
+    audio.addEventListener('ended', () => {
+        window.close();
+    });
 });
-
-// In case autoplay fails
-image.addEventListener('click', () => {
-    audio.play();
-})
